14205e695005e3095e23f511b5abbeae1cfce735
Paulo Coghi <paulo@coghi.com.br>
2018-04-09 20:17:53 -0300

    Rollup files were launched to space. Recovered.

---
+++ other/rollup/cli.js
@@ -0,0 +1,43 @@
+import json from 'rollup-plugin-json';
+import string from 'rollup-plugin-string';
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+// import uglify from 'rollup-plugin-uglify';
+// import { minify } from 'uglify-es';
+
+export default {
+	input: 'src/cli.js',
+	external: [
+		'assert',
+		'browser-sync',
+		'c.js',
+		'child_process',
+		'chokidar',
+		'constants',
+		'events',
+		'fs',
+		'os',
+		'path',
+		'rsyncwrapper',
+		'stream',
+		'svelte',
+		'uglify-es',
+		'url',
+		'util',
+		'vm'
+	],
+	output: {
+		file: 'cli.js',
+		format: 'cjs',
+		banner: '#!/usr/bin/env node'
+	},
+	plugins: [
+		json(),
+		string( { include: '**/*.md' } ),
+		nodeResolve( {
+			jsnext: true
+		} ),
+		commonjs(),
+		// uglify( {}, minify )
+	]
+};
