b27a400a4cf5fe3bba531907e9b6b42d0f5fd024
Paulo Coghi <paulo@coghi.com.br>
2018-11-06 16:41:09 -0200

    Reduce Cognitive Complexity on showError.js

--- src/utils/showError.js
+++ src/utils/showError.js
@@ -1,9 +1,11 @@
 const showError = function ( error, path, app = false ) {
 
-	let message = ( app ? '[Altiva generated code error] ' : '' ) +
-				( error.name ? error.name : 'Error' ) + ' in ' + path +
-				( ( error.start && error.start.line ) ? ', line ' + error.start.line : '' ) +
-				( ( error.start && error.start.column ) ? ', column ' + error.start.column : '' ) + ': ' + error.message
+	const app_message   = app ? '[Altiva generated code error] ' : ''
+	const error_name    = error.name ? error.name : 'Error'
+	const error_line    = ( error.start && error.start.line ) ? ', line ' + error.start.line : ''
+	const error_column  = ( error.start && error.start.column ) ? ', column ' + error.start.column : ''
+
+	let message = app_message + error_name + ' in ' + path + error_line + error_column + ': ' + error.message
 
 	console.log( message );
 };
